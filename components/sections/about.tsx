33de68bf78711addf7227bf75c67dedd90e9ba02
devr08 <148674860+devr08@users.noreply.github.com>
2023-10-30 04:35:09 +0000

    Pending changes exported from your codespace

--- components/sections/about.tsx
+++ components/sections/about.tsx
@@ -4,17 +4,17 @@ import Link from "next/link";
 
 const SectionAbout = () => {
   return (
-    <section className="flex flex-row-reverse items-center justify-beetween py-6 px-8">
+    <section className="flex flex-col-reverse md:flex-row-reverse items-center justify-beetween py-6 gap-6 px-8">
       <div>
-        <h1 className="text-3xl font-bold">
+        <h1 className="text-4xl font-bold">
           Mengenal Singkat <span className="text-primary">Tentang Kami</span>
         </h1>
-        <p className="text-sm text-muted-foreground mt-6 mb-2">
+        <p className="text-muted-foreground mt-6 mb-2">
           PT Lika Wira sakti berkedudukan di kota Tangerang yang
           didirikan pada tahun 2021 berdasarkan Akta Nomor 29 Tanggal 22
           Februari 2021, dibuat di hadapan Notaris Fikri, SH, MKN,
         </p>
-        <p className="text-sm text-muted-foreground mb-4">
+        <p className="text-muted-foreground mb-4">
           Dan juga merupakan perusahaan yang memiliki tenaga
           ahli dan berpengalaman di bidang pengamanan dengan didukung oleh SDM
           yang baik serta profesional
@@ -31,7 +31,7 @@ const SectionAbout = () => {
         alt="tentang pt lika wira sakti"
         height={600}
         width={1600}
-        className="w-[50%]"
+        className="w-[70%] md:w-[40%]"
       />
     </section>
   );
